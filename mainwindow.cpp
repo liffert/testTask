b2c34677ccf7fb8f911904ec3d0f7ef94a2f4e55
liffert <digitizenetwork@gmail.com>
2020-07-03 20:05:59 +0300

    add change to grid view functional

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,6 +18,7 @@ MainWindow::MainWindow(QWidget *parent)
     fillListWidgetView(contactListProvider->getActualContactList());
     ui->ContactListWidgetModel->setSpacing(1);
     ui->ContactListWidgetModel->setIconSize(QSize(iconSize, iconSize));
+    setListView();
     
     connect(callScreen, SIGNAL(rejected()), this, SLOT(cancelCall()));
 }
@@ -25,32 +26,25 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow() {
     delete ui;
     delete contactListProvider;
+    delete callScreen;
 }
 
 void MainWindow::fillListWidgetView(const QVector<QPair<QString, QString>> &list) {
     for(const auto &iter : list){
         QListWidgetItem *item = new QListWidgetItem();
-        
-        QImage image(iter.second);
-        image = image.scaled(iconSize, iconSize, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
-        QSize size = image.size();
-        
-
-        QPixmap map(size);
-        map.fill(Qt::GlobalColor::transparent);
-        QPainter painter(&map);
-        painter.setBrush(image);
-        painter.setPen(Qt::GlobalColor::white);
-        painter.drawRoundedRect(0, 0, iconSize, iconSize, iconSize / 2, iconSize / 2);
-        item->setIcon(QIcon(map));
-        
-        
+        item->setIcon(getItemIcon(iter.second));
         item->setText(iter.first);
-        item->setSizeHint(QSize(itemSize, itemSize));
         item->setTextAlignment(Qt::AlignmentFlag::AlignCenter);
         item->setBackground(QColor(77, 77, 77));
         item->setForeground(Qt::GlobalColor::white);
         ui->ContactListWidgetModel->addItem(item);
+    }
+}
+
+void MainWindow::setItemSize(const int size) {
+    QSize qsize(size, size);
+    for(int i = 0; i < ui->ContactListWidgetModel->count(); i++){
+        ui->ContactListWidgetModel->item(i)->setSizeHint(qsize);
     }
 }
 
@@ -79,8 +73,43 @@ void MainWindow::on_searchLine_textEdited(const QString &arg1) {
         if(ui->ContactListWidgetModel->item(i)->text().toStdString().find(arg1.toStdString()) == std::string::npos){
             ui->ContactListWidgetModel->item(i)->setHidden(true);
         }
-        else{
+        else {
             ui->ContactListWidgetModel->item(i)->setHidden(false);
         }
     }
+}
+
+void MainWindow::on_changeView_clicked() {
+    if(listView){
+        setGridView();
+    }
+    else{
+        setListView();   
+    }
+    listView = !listView;
+}
+
+
+QIcon MainWindow::getItemIcon(const QString &path) {
+    QImage image(path);
+    image = image.scaled(iconSize, iconSize, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+    QSize size = image.size();
+    QPixmap map(size);
+    map.fill(Qt::GlobalColor::transparent);
+    QPainter painter(&map);
+    painter.setBrush(image);
+    painter.setPen(Qt::GlobalColor::white);
+    painter.drawRoundedRect(0, 0, iconSize, iconSize, iconSize / 2, iconSize / 2);
+    return QIcon(map);
+}
+
+void MainWindow::setListView() {
+    ui->ContactListWidgetModel->setViewMode(QListWidget::ListMode);
+    setItemSize(listItemSize);
+}
+
+void MainWindow::setGridView() {
+    ui->ContactListWidgetModel->setViewMode(QListWidget::IconMode);
+    ui->ContactListWidgetModel->setDragDropMode(QListWidget::NoDragDrop);
+    setItemSize(gridItemZize);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -18,15 +18,21 @@ public:
 private slots:
     void on_ContactListWidgetModel_clicked(const QModelIndex &index);
     void cancelCall();
-    
     void on_searchLine_textEdited(const QString &arg1);
-    
+    void on_changeView_clicked();
+
 private:
     Ui::MainWindow *ui;
     QMessageBox *callScreen;
     ContactListProvider * contactListProvider;
     QStringListModel *model;
     void fillListWidgetView(const QVector<QPair<QString, QString>> &list);
-    const int itemSize = 50;
+    QIcon getItemIcon(const QString &path);
+    const int listItemSize = 50;
+    const int gridItemZize = 75;
     const int iconSize = 40;
+    void setItemSize(const int size);
+    bool listView = true;
+    void setListView();
+    void setGridView();
 };
