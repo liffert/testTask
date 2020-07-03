352073572e4951668eae66499de69b1b08fcc8dc
liffert <digitizenetwork@gmail.com>
2020-07-03 16:27:51 +0300

    add functional: call, search, show rounded pictures

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,16 +2,24 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 #include <QListWidget>
+#include <QPainter>
+#include <QPixmap>
+#include <QImage>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
       ui(new Ui::MainWindow) {
     
     ui->setupUi(this);
+    callScreen = new QMessageBox(this);
+    callScreen->setStandardButtons(QMessageBox::Cancel);
+    
     contactListProvider = new ContactListProvider();
     fillListWidgetView(contactListProvider->getActualContactList());
-    //ui->ContactListWidgetModel->item(1)->setHidden(true);
     ui->ContactListWidgetModel->setSpacing(1);
+    ui->ContactListWidgetModel->setIconSize(QSize(iconSize, iconSize));
+    
+    connect(callScreen, SIGNAL(rejected()), this, SLOT(cancelCall()));
 }
 
 MainWindow::~MainWindow() {
@@ -19,10 +27,21 @@ MainWindow::~MainWindow() {
     delete contactListProvider;
 }
 
-void MainWindow::fillListWidgetView(const QStringList &list) {
+void MainWindow::fillListWidgetView(const QVector<QPair<QString, QString>> &list) {
     for(const auto &iter : list){
         QListWidgetItem *item = new QListWidgetItem();
-        item->setText(iter);
+        
+        QImage image(iter.second);
+        QPixmap map(iconSize, iconSize);
+        map.fill(Qt::GlobalColor::transparent);
+        QPainter painter(&map);
+        painter.setBrush(image);
+        painter.setPen(Qt::GlobalColor::white);
+        painter.drawRoundedRect(0, 0, iconSize, iconSize, iconSize / 2, iconSize / 2);
+        item->setIcon(QIcon(map));
+        
+        
+        item->setText(iter.first);
         item->setSizeHint(QSize(itemSize, itemSize));
         item->setTextAlignment(Qt::AlignmentFlag::AlignCenter);
         item->setBackground(QColor(77, 77, 77));
@@ -33,7 +52,31 @@ void MainWindow::fillListWidgetView(const QStringList &list) {
 
 
 void MainWindow::on_ContactListWidgetModel_clicked(const QModelIndex &index) {
-    qDebug() << index;
-    //std::this_thread::sleep_for(std::chrono::seconds(2));
-    //ui->ContactListWidgetModel->item(index.row())->setSelected(false);
+    callScreen->setText("Calling to " + ui->ContactListWidgetModel->item(index.row())->text());
+    callScreen->show();
+    contactListProvider->call(index.row());
+    callScreen->hide();
+    
+    ui->ContactListWidgetModel->item(index.row())->setSelected(false);
+}
+
+void MainWindow::cancelCall() {
+    contactListProvider->endCall();
+    callScreen->hide();
+}
+
+void MainWindow::on_searchLine_textEdited(const QString &arg1) {
+    if(arg1 == ""){
+        for(int i = 0; i < ui->ContactListWidgetModel->count(); i++){
+            ui->ContactListWidgetModel->item(i)->setHidden(false);
+        }
+    }
+    for(int i = 0; i < ui->ContactListWidgetModel->count(); i++){
+        if(ui->ContactListWidgetModel->item(i)->text().toStdString().find(arg1.toStdString()) == std::string::npos){
+            ui->ContactListWidgetModel->item(i)->setHidden(true);
+        }
+        else{
+            ui->ContactListWidgetModel->item(i)->setHidden(false);
+        }
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 
 #include <QMainWindow>
 #include "contactlistprovider.h"
+#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,11 +17,16 @@ public:
     
 private slots:
     void on_ContactListWidgetModel_clicked(const QModelIndex &index);
+    void cancelCall();
+    
+    void on_searchLine_textEdited(const QString &arg1);
     
 private:
     Ui::MainWindow *ui;
+    QMessageBox *callScreen;
     ContactListProvider * contactListProvider;
     QStringListModel *model;
-    void fillListWidgetView(const QStringList &list);
+    void fillListWidgetView(const QVector<QPair<QString, QString>> &list);
     const int itemSize = 50;
+    const int iconSize = 40;
 };
