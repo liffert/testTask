a16b807b5b59c9556cc821f12b8514b23ddd6517
liffert <digitizenetwork@gmail.com>
2020-07-09 22:25:16 +0300

    add bool favorite class field

--- contact.cpp
+++ contact.cpp
@@ -1,6 +1,7 @@
 #include "contact.h"
 
-Contact::Contact(const QString name, const QString iconPath, const int id) : name(name), iconPath(iconPath), id(id) {}
+Contact::Contact(const QString name, const QString iconPath, const int id, const bool favorite) 
+    : name(name), iconPath(iconPath), id(id), favorite(favorite) {}
 
 QString Contact::getName() const {
     return name;
@@ -12,4 +13,12 @@ QString Contact::getIconPath() const {
 
 int Contact::getId() const {
     return id;
+}
+
+bool Contact::isFavorite() const {
+    return favorite;
+}
+
+void Contact::setFavorite(const bool favorite) {
+    this->favorite = favorite;
 }
--- contact.h
+++ contact.h
@@ -6,10 +6,13 @@ private:
     QString name;
     QString iconPath;
     int id;
+    bool favorite;
 public:
-    Contact(const QString name, const QString iconPath, const int id);
+    Contact(const QString name, const QString iconPath, const int id, const bool favorite);
     QString getName() const;
     QString getIconPath() const;
     int getId() const;
+    bool isFavorite() const;
+    void setFavorite(const bool favorite);
 };
 
